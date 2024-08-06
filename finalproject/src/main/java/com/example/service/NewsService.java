8a875eb7e5e97bc5fd2684de0f9a6268c4072315
humanwade <165367827+humanwade@users.noreply.github.com>
2024-08-06 10:01:01 +0900

    Delete finalproject directory

--- finalproject/src/main/java/com/example/service/NewsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import java.util.List;
-
-import com.example.domain.NewsVO;
-
-public interface NewsService {
-	List<NewsVO> getNewsList(Integer start, String search);
-}
--- finalproject/src/main/java/com/example/service/PhotoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.domain.PhotosVO;
-import com.example.domain.UserPhotoVO;
-import com.example.domain.UserVO;
-
-public interface PhotoService {
-	public void insertPhoto(PhotosVO vo);
-}
